1a0e7dcb6ff344e36c9039c18c33c8fe1d773caf
Bruno Giminiani <giminiani@gmail.com>
2020-02-21 16:04:13 -0300

    feat: Add style guide

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,12 @@
-const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
-routes.get('/', (req, res) => res.json({ it_is_still_working: true }));
+routes.get('/', (req, res) =>
+  res.json({
+    it_is_still_working:
+      'Yep, this is a style guide test made after prettier installation',
+  })
+);
 
 export default routes;
