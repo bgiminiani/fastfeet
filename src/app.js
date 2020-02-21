4fc83bc264c825bfbd32df71abb258ff19941714
Bruno Giminiani <giminiani@gmail.com>
2020-02-21 11:49:57 -0300

    feat: add sucrase

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
-const express = require('express');
+import express from 'express';
 
 const app = express();
 
-app.get('/', (req, res) => res.json({ working: true }));
+app.get('/', (req, res) => res.json({ is_it_working: true }));
 
 app.listen(3333);
 
