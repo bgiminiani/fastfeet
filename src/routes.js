0582867abb2c3dba80d7226ef2924941b40f3901
Bruno Giminiani <giminiani@gmail.com>
2020-02-26 19:25:26 -0300

    feat: Add user controle store

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,8 @@
 import { Router } from 'express';
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', (req, res) =>
-  res.json({
-    it_is_still_working:
-      'Yep, this is a style guide test made after prettier installation',
-  })
-);
-
-routes.post('/users', async (req, res) => {
-  const user = await User.create(req.body);
-  return res.json({ user_created: user });
-});
+routes.post('/users', UserController.store);
 
 export default routes;
